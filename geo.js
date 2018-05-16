847cf52428fb396633525876a2d636f0d7913425
julianPescobar <magneticrex@gmail.com>
2018-05-16 09:22:36 -0300

    se agregan los kilometros de distancia, resta la traduccion

--- geo.js
+++ geo.js
@@ -5,30 +5,29 @@ var assert = require('assert')
 var url = 'mongodb://localhost/'
 var geodb = 'geodb'
 var app = express()
+const https = require('https')
+const fs = require('fs')
+const port = 3000
 
 // Conectamos con database mongodb
 mongo.connect(url, function (err, client) {
   assert.equal(null, err)
-
-  var db = client.db(geodb)
+// var db = client.db(geodb)
 })
-// -------------------------------
+// ----------------------------------------------
 
 // Funcion para insertar varios Jsons en la database de mongo
 const insertDocuments = function (db, callback) {
   // Get the documents collection
   const collection = db.collection('markers')
   // Insert some documents
-  collection.insertMany([
-    {lat: 20.02, lng: 33.03, tipomarker: 'Dealer', nombre: 'Pepe argento', direccion: 'Sargento Cabral 1239', telefono: '4361-2225', icono: '/images/0.jpg'},
-    {lat: 10.023, lng: 14.022, tipomarker: 'Artista', nombre: 'Test Artista', icono: '/images/6.jpg'}
-  ], function (err, result) {
+  collection.insertMany([{test: 'test'}], function (err, result) {
     assert.equal(err, null)
-    console.log('Inserted 2 documents into the collection')
+    console.log('Inserted Document(s)')
     callback(result)
   })
 }
-// -----------------------------------------------------------
+// ---------------------------------------------
 // Funcion para leer todo de una base de datos
 const buscar = function (db, callback) {
   // Get the documents collection
@@ -36,7 +35,8 @@ const buscar = function (db, callback) {
   // Find some documents
   coll.find({}).toArray(function (err, docs) {
     assert.equal(err, null)
-    console.log('Found records')
+    // console.log('Found records')
+
     // console.log(docs)
     callback(docs)
   })
@@ -47,27 +47,34 @@ const buscar = function (db, callback) {
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-// -----------------------
-
+// ------------------------------------------
 // Handleamos el acceso a localhost:3000/
 app.get('/', function (req, res) {
+  // test googleMapsClient
+  // ----------------------------------------
   // nos conectamos a la base
   mongo.connect(url, function (err, client) {
     assert.equal(null, err)
-    console.log('conexion exitosa con mongodb')
+    // console.log('conexion exitosa con mongodb')
     var db = client.db(geodb)
     var coll = db.collection('markers')
-    //buscamos en markers
-    coll.find({}).toArray(function (err,docs){
-      assert.equal(err,null)
-console.log(docs)
+    // buscamos en markers
+    coll.find({}).toArray(function (err, docs) {
+      assert.equal(err, null)
+      // console.log(docs)
       res.render('map', {markers: docs})
     })
-    })
-
-
+  })
 })
-// --------------------------------------
+// -----------------------------------------
 
 // Abrimos el puerto 3000 para que arranque la app
-app.listen(3000, () => console.log('Escuchando en puerto 3000'))
+
+// app.listen(3000, () => console.log('Escuchando en puerto 3000'))
+const httpsOptions = {
+  key: fs.readFileSync('public/key-20180514-102809.pem'),
+  cert: fs.readFileSync('public/cert-20180514-102809.crt')
+}
+const server = https.createServer(httpsOptions, app).listen(port, () => {
+  console.log('server running at ' + port)
+})
